87d6dd0504b354598e9109de1e608166e5164d58
Roland Lohner <loczaj@gmail.com>
2013-12-06 00:25:38 +0100

    Proper impact parameter found

--- experiment.cpp
+++ experiment.cpp
@@ -1,8 +1,10 @@
 #include "experiment.hpp"
 #include "experiments/collision-aph-proton.hpp"
+#include "experiments/sandbox.hpp"
 
 int main(int argc, char* argv[]) {
 
-	CollisionAbrinesPercivalHydrogenWithProton experiment(0.5, 50);
-	return experiment.carryOut(experiment, 100);
+	// SandboxExperiment experiment;
+	CollisionAbrinesPercivalHydrogenWithProton experiment(15, 50);
+	return experiment.carryOut(experiment, 10000);
 }
