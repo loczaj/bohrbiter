ea6391007525a201561e9b6db8aa5bf4955d7aa6
Roland Lohner <loczaj@gmail.com>
2013-12-12 11:30:59 +0100

    Helium experiment added

--- experiment.cpp
+++ experiment.cpp
@@ -1,11 +1,12 @@
 #include "experiment.hpp"
 #include "experiments/collision-h-proton.hpp"
+#include "experiments/collision-he-proton.hpp"
 #include "experiments/sandbox.hpp"
 
 int main(int argc, char* argv[]) {
 
 	// SandboxExperiment experiment;
-	CollisionAbrinesPercivalHydrogenWithProton experiment(15, 50, 1e-10, 1e-10, 1e-7);
-	// HeliumExperiment experiment;
-	return experiment.carryOut(experiment, 10000);
+	CollisionAbrinesPercivalHydrogenWithProton experiment(15, 50, 1e-9, 1e-9, 1e-7);
+	// CollisionHeliumWithProton experiment;
+	return experiment.carryOut(experiment, 5000);
 }
---
+++ experiments/collision-he-proton.hpp
@@ -0,0 +1,57 @@
+#ifndef HELIUM_HPP
+#define HELIUM_HPP
+
+#include <boost/numeric/odeint.hpp>
+#include <simulbody/simulator.hpp>
+
+#include "../abrines-percival.hpp"
+#include "../experiment.hpp"
+
+class CollisionHeliumWithProton: public Experiment {
+
+	std::ofstream stream;
+
+	System bbsystem;
+	PositionPrintField printField;
+
+	AbrinesPercivalAtom *helium = nullptr;
+
+public:
+
+	int open(int numberOfRounds, bool seedRandom) {
+		helium = new AbrinesPercivalAtom(&bbsystem, Element::He, 4.00260325);
+		return 0;
+	}
+
+	int run(int round, bool tracking, bool skipUntracked) {
+		runge_kutta_dopri5<Phase> stepper;
+		auto ctrdStepper = make_controlled(1e-10, 1e-10, stepper);
+		Simulator<decltype(ctrdStepper)> simulator(ctrdStepper, &bbsystem);
+
+		helium->randomize(randomEngine);
+		//helium->install();
+
+		Printer printer("helium-" + std::to_string(round) + ".csv");
+		printer.addField(new PositionPrintField());
+		simulator.setPrinter(printer);
+
+		double energy = bbsystem.getSystemEnergy();
+		double time = simulator.simulate(0.0, 200.0, 0.0001);
+
+		if (time < 0.0)
+			return -1;
+
+		if (abs((energy - bbsystem.getSystemEnergy()) / energy) > 1e-6)
+			return -2;
+
+		return 0;
+	}
+
+	int close(int successfulRounds) {
+		return 0;
+	}
+
+};
+
+#endif /* HELIUM_HPP */
+
