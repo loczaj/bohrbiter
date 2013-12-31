e14fefa78ad5586df54c074900d2205d531157ba
Roland Lohner <loczaj@gmail.com>
2013-12-31 18:33:38 +0100

    Fix Heisenberg potential: use electron momentum in respect of nucleus

--- kirschbaum-wilets.hpp
+++ kirschbaum-wilets.hpp
@@ -17,29 +17,15 @@ private:
 	double mu;
 	double xi, xi2, xi4;
 
-	double earthMass;
-	double moonMass;
-
 	double r2 = 0, r4 = 0;
 	double p2 = 0, p4 = 0;
 	double exponent = 0, factor = 0;
 
 	vector3D p, pEarth;
 
-	void calculateP4(const Phase& phase) {
-		p = phase.getBodyVelocity(moon);
-		pEarth = phase.getBodyVelocity(earth);
-		p *= moonMass;
-		pEarth *= earthMass;
-		p -= pEarth;
-		p2 = p.scalarProduct(p);
-		p4 = p2 * p2;
-	}
-
 public:
-	HeisenbergInteraction(double alpha, double mu, double xi, double earthMass, double moonMass,
-			identifier earth, identifier moon)
-			: alpha(alpha), mu(mu), xi(xi), earthMass(earthMass), moonMass(moonMass) {
+	HeisenbergInteraction(double alpha, double mu, double xi, identifier earth, identifier moon)
+			: alpha(alpha), mu(mu), xi(xi) {
 		xi2 = xi * xi;
 		xi4 = xi2 * xi2;
 		this->setBodies(earth, moon);
@@ -51,7 +37,9 @@ public:
 		r2 = r.scalarProduct(r);
 		r4 = r2 * r2;
 
-		calculateP4(phase);
+		calculateV(phase);
+		p2 = v.scalarProduct(v);
+		p4 = p2 * p2;
 
 		exponent = exp(alpha * (1 - r4 * p4 / xi4));
 		factor = xi2 * exponent / (2 * alpha * mu * r4) + p4 * exponent / (xi2 * mu);
@@ -68,7 +56,9 @@ public:
 		r2 = r.scalarProduct(r);
 		r4 = r2 * r2;
 
-		calculateP4(phase);
+		calculateV(phase);
+		p2 = v.scalarProduct(v);
+		p4 = p2 * p2;
 
 		return xi2 * exp(alpha * (1 - r4 * p4 / xi4)) / (4 * alpha * r2 * mu);
 	}
@@ -132,9 +122,7 @@ public:
 					interactions.push_back(new CoulombInteraction(1.0, e1, e2));
 			}
 			interactions.push_back(new CoulombInteraction(-1.0 * nucleusCharge, nucleus, e1));
-			interactions.push_back(
-					new HeisenbergInteraction(5, reducedMass, 1.0488, nucleusMass, electronMass, nucleus,
-							e1));
+			interactions.push_back(new HeisenbergInteraction(5, reducedMass, 1.0488, nucleus, e1));
 		}
 
 		for (Interaction* interaction : interactions) {
