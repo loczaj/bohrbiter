a8f3a1fe8e257eb045b4846a4e384af27c573b61
Roland Lohner <loczaj@gmail.com>
2013-12-31 18:35:08 +0100

    Work on p+He experiment

--- experiments/collision-he-proton.hpp
+++ experiments/collision-he-proton.hpp
@@ -18,15 +18,15 @@ class CollisionHeliumWithProton: public Experiment {
 
 public:
 
-	int open(int numberOfRounds, bool seedRandom) {
+	int open(int numberOfRounds, bool seedRandom) override {
 		helium = new KirschbaumWiletsAtom(&bbsystem, Element::He, 4.00260325);
 		return 0;
 	}
 
-	int run(int round, bool tracking, bool skipUntracked) {
+	int run(int round, bool tracking, bool skipUntracked) override {
 		runge_kutta_dopri5<Phase> stepper;
-		auto ctrdStepper = make_controlled(1e-10, 1e-10, stepper);
-		Simulator<decltype(ctrdStepper)> simulator(ctrdStepper, &bbsystem);
+		auto ctrstepper = make_controlled(1e-6, 1e-6, stepper);
+		Simulator<decltype(ctrstepper)> simulator(ctrstepper, &bbsystem);
 
 		//helium->randomize(randomEngine);
 		helium->install();
@@ -35,19 +35,39 @@ public:
 		printer.addField(new PositionPrintField());
 		simulator.setPrinter(printer);
 
+		cout << "Electron config: " << static_cast<int>(helium->electronConfiguration) << endl;
+		cout << "Nucleus element: " << static_cast<int>(helium->nucleusElement) << endl;
+		cout << "Nucleus charge: " << helium->nucleusCharge << endl;
+		cout << "Nucleus mass: " << helium->nucleusMass << endl;
+		cout << "Reduced mass: " << helium->reducedMass << endl;
+		cout << "Number of bodies: " << helium->getBodies().size() << endl;
+		cout << "Orbits: ";
+		for (string orbit : helium->orbitNames) {
+			cout << orbit << " ";
+		}
+		cout << endl << endl;
+
+		cout << "Energy: " << bbsystem.getSystemEnergy() << endl;
+		cout << "  1s1 : " << helium->getOrbitalEnergy("1s1") << endl;
+		cout << "  1s2 : " << helium->getOrbitalEnergy("1s2") << endl << endl;
+
 		double energy = bbsystem.getSystemEnergy();
-		double time = simulator.simulate(0.0, 200.0, 0.0001);
+		double time = simulator.simulate(0.0, 220, 0.0001);
+
+		cout << "Energy: " << bbsystem.getSystemEnergy() << endl;
+		cout << "  1s1 : " << helium->getOrbitalEnergy("1s1") << endl;
+		cout << "  1s2 : " << helium->getOrbitalEnergy("1s2") << endl;
 
 		if (time < 0.0)
 			return -1;
 
-		if (abs((energy - bbsystem.getSystemEnergy()) / energy) > 1e-6)
+		if (abs((energy - bbsystem.getSystemEnergy()) / energy) > 1e-4)
 			return -2;
 
 		return 0;
 	}
 
-	int close(int successfulRounds) {
+	int close(int successfulRounds) override {
 		return 0;
 	}
 
